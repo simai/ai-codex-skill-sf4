6b46bfd817ebb4ab6cc3691dfe2314ca2b2bcfb1
Rim Zabarov <rim@zabarov.com>
2026-06-08 12:12:34 +0300

    Stop tracking local source artifacts

--- source/output/wizard-skeleton/simai.example/master/simai.example/lang/ru/.wizard.config.php
+++
@@ -1,9 +0,0 @@
-<?
-$MESS["WIZARD_SOLUTION"] = "SIMAI Example";
-$MESS["SITE_CHOICE"] = "Выбор сайта";
-$MESS["CREATE_IBLOCKCONFIG"] = "Подготовка конфигурации инфоблоков";
-$MESS["ADD_URLREWRITE"] = "Подготовка правил URL";
-$MESS["CREATE_IBLOCK_TYPE"] = "Подготовка типов инфоблоков";
-$MESS["IMPORT_IBLOCK_ARCHIVE"] = "Подготовка архива инфоблока";
-$MESS["FINISH"] = "Завершение";
-$MESS["FINISH_MESSAGE"] = "Skeleton мастера подготовлен. Перед live запуском требуется аудит, backup и rollback.";
